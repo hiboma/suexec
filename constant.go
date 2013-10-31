ba8b52dd56172d0b48ae71889b5eeb2243384b72
id:hiboma(ひろや) <hiroyan@gmail.com>
2013-10-31 16:30:20 +0900

    refactor: 定数を分離

---
+++ constant.go
@@ -0,0 +1,7 @@
+package suexec
+
+const AP_HTTPD_USER = "vagrant"
+const AP_UID_MIN = "501"
+const AP_GID_MIN = "501"
+const AP_DOC_ROOT = "/vagrant/misc"
+const AP_LOG_EXEC = "/tmp/suexec.log"
--- suexec.go
+++ suexec.go
@@ -9,12 +9,6 @@ import (
 	"syscall"
 )
 
-const AP_HTTPD_USER = "vagrant"
-const AP_UID_MIN = "501"
-const AP_GID_MIN = "501"
-const AP_DOC_ROOT = "/vagrant/misc"
-const AP_LOG_EXEC = "/tmp/suexec.log"
-
 func IsUserdirEnabled(username string) bool {
 	return strings.HasPrefix(username, "~")
 }
