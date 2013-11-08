36a77caca0a5342963adcf82745377397e61a980
id:hiboma(ひろや) <hiroyan@gmail.com>
2013-11-08 12:31:08 +0900

    fix bug: printf format type is invalid.

--- constant.go
+++ constant.go
@@ -12,9 +12,9 @@ const AP_LOG_EXEC = "/tmp/suexec.log"
 
 func PrintConstants() {
 	fmt.Printf(" -D AP_DOC_ROOT=%s\n", AP_DOC_ROOT)
-	fmt.Printf(" -D AP_GID_MIN=%s\n", AP_GID_MIN)
+	fmt.Printf(" -D AP_GID_MIN=%d\n", AP_GID_MIN)
 	fmt.Printf(" -D AP_HTTPD_USER=%s\n", AP_HTTPD_USER)
 	fmt.Printf(" -D AP_LOG_EXEC=%s\n", AP_LOG_EXEC)
 	fmt.Printf(" -D AP_SAFE_PATH=%s\n", AP_SAFE_PATH)
-	fmt.Printf(" -D AP_UID_MIN=%s\n", AP_UID_MIN)
+	fmt.Printf(" -D AP_UID_MIN=%d\n", AP_UID_MIN)
 }
